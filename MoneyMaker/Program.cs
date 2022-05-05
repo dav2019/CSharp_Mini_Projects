0d1b0847754fb47f3cb3e0e3de85ab52c0b97197
David Bui <dav_edu@tutamail.com>
2022-05-05 16:42:05 +1000

    money maker

---
+++ MoneyMaker/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MoneyMaker
+{
+  class MainClass
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to Money Maker!");
+      Console.WriteLine("Enter an amount to convert to coins:");
+      string totalAsString = Console.ReadLine();
+      double total = Convert.ToDouble(totalAsString);
+      Console.WriteLine($"{totalAsString} cents is equal to...");
+
+      // Define coin value
+      int goldValue = 10;
+      int silverValue = 5;
+
+      // Calculate the change
+      double goldCoins = Math.Floor(total/goldValue);
+      double remainder = total % goldValue;
+      // Console.WriteLine(goldCoins);
+      // Console.WriteLine(remainder);
+
+      double silverCoins = Math.Floor(remainder/silverValue);
+      remainder = remainder % silverValue;
+
+      // Print results
+      Console.WriteLine($"Gold coins: {goldCoins}");
+      Console.WriteLine($"Silver coins: {silverCoins}");
+      Console.WriteLine($"Bronze coins: {remainder}");
+    }
+  }
+}
