891a27396d6aee5517b90606cc54fd8c8dbe1b72
David Bui <dav_edu@tutamail.com>
2022-05-19 12:19:42 +1000

    OOYA

---
+++ OOYA/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OOYA
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Profile sam = new Profile("Sam Drakkila", 30, "New York", "USA", "he/him");
+      sam.SetHobbies(new string[] {
+        "Movies",
+        "Tennis",
+        "Mountain biking"
+      });
+      Console.WriteLine(sam.ViewProfile());
+    }
+  }
+}
