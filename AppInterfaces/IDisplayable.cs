af93934e543486dc33816c425ad17b5f62a4f90e
David Bui <dav_edu@tutamail.com>
2022-05-22 16:38:37 +1000

    App Interfaces

---
+++ AppInterfaces/IDisplayable.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace AppInterfaces
+{
+  interface IDisplayable
+  {
+    void Display();
+  }
+
+}
---
+++ AppInterfaces/IResetable.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace AppInterfaces
+{
+  interface IResetable
+  {
+    void Reset();
+  }
+
+}
