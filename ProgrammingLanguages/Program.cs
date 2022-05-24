388f5033c3dd0e0d597e65437066e2a68d491f1d
David Bui <dav_edu@tutamail.com>
2022-05-24 18:28:01 +1000

    PL

---
+++ ProgrammingLanguages/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ProgrammingLanguages
+{
+  class Program
+  {
+    static void Main()
+    {
+      List<Language> languages = File.ReadAllLines("./languages.tsv")
+        .Skip(1)
+        .Select(line => Language.FromTsv(line))
+        .ToList();
+
+        foreach (var lang in languages)
+        {
+          // Console.WriteLine(lang.Prettify());
+        }
+
+        var basicInfos = languages.Select(x => $"{x.Year}, {x.Name}, {x.ChiefDeveloper}");
+
+        foreach (var lang in basicInfos)
+        {
+          // Console.WriteLine(lang);
+        }
+
+        var csharpLangs = languages.Where(x => x.Name == "C#");
+
+         foreach (var lang in csharpLangs)
+        {
+          // Console.WriteLine(lang.Prettify());
+        }
+
+        var microsoftLangs = languages.Where(x => x.ChiefDeveloper.Contains("Microsoft"));
+
+         foreach (var lang in microsoftLangs)
+        {
+          // Console.WriteLine(lang.Prettify());
+        }
+
+        var lispLangs = languages.Where(x => x.Predecessors.Contains("Lisp"));
+
+         foreach (var lang in lispLangs)
+        {
+          // Console.WriteLine(lang.Prettify());
+        }
+
+        var scriptLangs = languages
+          .Where(x => x.Name.Contains("Script"))
+          .Select(x => x.Name);
+
+         foreach (var lang in scriptLangs)
+        {
+          // Console.WriteLine(lang);
+        }
+
+        // Console.WriteLine($"Number of languages: {languages.Count}");
+
+        var nearMilleniumLangs = languages
+          .Where(x => x.Year >= 1995 && x.Year <= 2005)
+          .Select(x => $"{x.Name} was invented in {x.Year}");
+
+        // Console.WriteLine($"Near millenium languages: {nearMilleniumLangs.Count()}");
+        foreach (var lang in nearMilleniumLangs)
+        {
+          // Console.WriteLine(lang);
+        }
+
+      // PrettyPrintAll(lispLangs);
+        // PrintAll(nearMilleniumLangs);
+
+        var ordered = languages.OrderBy(x => x.Name);
+        // PrettyPrintAll(ordered);
+
+        var oldest = languages.Min(x => x.Year);
+        Console.WriteLine(oldest);
+    }
+
+    public static void PrettyPrintAll(IEnumerable<Language> langs)
+    {
+      foreach (Language lang in langs)
+      {
+        Console.WriteLine(lang.Prettify());
+      }
+    }
+
+    public static void PrintAll(IEnumerable<Object> sequence)
+    {
+      foreach(Object obj in sequence)
+      {
+        Console.WriteLine(obj);
+      }
+    }
+  }
+}
